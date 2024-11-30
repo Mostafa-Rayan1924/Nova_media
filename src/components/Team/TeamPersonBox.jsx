12f9aa50f5d55eaa9c91e7b3a4cf969c35947517
Mostafa rayan <mr3309296@gmail.com>
2024-11-30 20:35:27 +0200

    add api to team page2

--- src/components/Team/TeamPersonBox.jsx
+++ src/components/Team/TeamPersonBox.jsx
@@ -1,63 +1,59 @@
 "use client";
 import { ChartBarStacked, MailCheck, Phone } from "lucide-react";
 import { motion } from "framer-motion";
-const TeamPersonBox = ({ person }) => {
+const TeamPersonBox = ({ person, index }) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: -30 }}
-      whileInView={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
-      className="group relative block h-[350px] rounded-lg overflow-hidden  bg-black">
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.3, delay: index * 0.2 },
+      }}
+      className="group relative block mt-10  border border-border hover:border-primary duration-200 rounded-lg   bg-black">
       <img
         alt={person?.name}
         src={person?.image}
-        className="absolute  inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        className="size-20 absolute -top-10 left-4 rounded-full object-cover "
       />
 
-      <div className="relative px-4 py-4 ">
-        <div className="bg-accent/50 p-1 rounded-sm">
-          <p className="text-lg  font-semibold uppercase tracking-widest text-primary">
-            {person?.name}
-          </p>
-
-          <p className=" whitespace-nowrap  font-bold text-foreground ">
-            {person?.qualification}
-          </p>
-        </div>
-        <div className="mt-40  ">
-          <div className="translate-y-8 space-y-2 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-            <h4
-              className="
+      <div className="relative space-y-2  p-4 ">
+        <p className="text-lg  font-semibold uppercase tracking-widest text-primary">
+          {person?.name}
+        </p>
+        <p className=" whitespace-nowrap  font-bold text-foreground ">
+          {person?.qualification}
+        </p>
+        <h4
+          className="
             flex
             items-center
             gap-2
-            
             text-white
             leading-relaxed">
-              <Phone className="text-primary" size={20} />
-              {person?.phone}
-            </h4>
-            <h5
-              className="
+          <Phone className="text-primary" size={20} />
+          {person?.phone}
+        </h4>
+        <h5
+          className="
             text-white
             flex
             items-center
             gap-2
             leading-relaxed   ">
-              <MailCheck className="text-primary" size={20} />
-              {person?.email}
-            </h5>
-            <h6
-              className="
+          <MailCheck className="text-primary" size={20} />
+          {person?.email}
+        </h5>
+        <h6
+          className="
             text-white
             flex
             items-center
             gap-2
             leading-relaxed  ">
-              <ChartBarStacked className="text-primary" size={20} />
-              {person?.department}
-            </h6>
-          </div>
-        </div>
+          <ChartBarStacked className="text-primary" size={20} />
+          {person?.department}
+        </h6>
       </div>
     </motion.div>
   );
--- src/components/Team/TeamsSec.jsx
+++ src/components/Team/TeamsSec.jsx
@@ -23,13 +23,19 @@ const TeamsSec = () => {
           Array.from({ length: 6 }).map((item, index) => (
             <div
               key={index}
-              className="h-[350px]  bg-accent animate-pulse rounded-lg relative">
-              <div className="absolute top-4 right-4 w-36 h-4 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
-              <div className="absolute top-12 right-4 w-40 h-3 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+              className="h-[200px] mt-10  bg-accent animate-pulse rounded-lg relative">
+              <div className="size-20 absolute -top-10 left-4 rounded-full bg-gray-300 dark:bg-black/40 animate-pulse"></div>
+              <div className="space-y-4 mt-4 p-4">
+                <div className="   w-36 h-4 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+                <div className="   w-40 h-3 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+                <div className="  w-20 h-2 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+                <div className="  w-44 h-3 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+                <div className="  w-28 h-4 bg-gray-300 dark:bg-black/40 animate-pulse rounded-lg"></div>
+              </div>
             </div>
           ))}
-        {data.slice(1).map((person) => (
-          <TeamPersonBox key={person?._id} person={person} />
+        {data.slice(1).map((person, index) => (
+          <TeamPersonBox key={person?._id} person={person} index={index} />
         ))}
       </div>
     </section>
