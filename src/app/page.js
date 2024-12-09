8ba3b0e6a226a3f308bec162f7d10608ca590d25
Mostafa rayan <mr3309296@gmail.com>
2024-12-09 13:23:36 +0200

    seo v1

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Head from "next/head"; // تأكد من استيراد Head هنا
 import About from "@/components/Home/About";
 import Hero from "@/components/Home/Hero";
 import LatestProjects from "@/components/Home/LatestProjects";
 import OurCustomers from "@/components/Home/OurCustomers";
 import Services from "@/components/Home/Services";
+
 // في ملف الصفحة الرئيسية، مثل page.js
 export const metadata = {
   title: "نوفا ميديا - الصفحة الرئيسية",
@@ -10,12 +12,18 @@ export const metadata = {
     "نوفا ميديا تقدم مجموعة شاملة من خدمات الطباعة الورقية والأقمشة، وتقنيات الليزر والفايبر، بالإضافة إلى الطباعة الداخلية والخارجية، والتسويق الإلكتروني لجعل أفكارك حقيقة.",
   keywords:
     "نوفا ميديا,nova,novamedia,novmedia-eg,شركة دعايا واعلان, خدمات الطباعة, طباعة ورقية, طباعة الأقمشة, تقنيات الليزر, تقنيات الفايبر, الطباعة الداخلية, الطباعة الخارجية, التسويق الإلكتروني, أفضل خدمات الطباعة",
-  verification: "yRJxt4zBwj5HVHAibjsaEKY5jV3smdQTV7bZ6FHAyGI",
 };
 
 export default function Home() {
   return (
     <main className="my-[140px] container space-y-28 md:space-y-36">
+      <Head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="yRJxt4zBwj5HVHAibjsaEKY5jV3smdQTV7bZ6FHAyGI"
+        />
+      </Head>
       <Hero />
       <About />
       <Services />
