9b0dbe7e0dacbc1a32a5799b492a396e38e2444c
Mostafa rayan <mr3309296@gmail.com>
2024-12-09 12:43:36 +0200

    seo v

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -8,31 +8,30 @@ export async function GET() {
     "exhibitions", // صفحة "المعارض"
     "contactus", // صفحة "اتصل بنا"
     "team", // صفحة " فريق العمل"
-    "job-ads", // "صفحة اعلان الوظائف",
-    "site-ads", // "صفحة اعلان الوظائف",
+    "job-ads", // "صفحة اعلان الوظائف"
+    "site-ads", // "صفحة اعلان الوظائف"
   ];
 
   // إنشاء خريطة الموقع بصيغة XML
-  const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${staticPages
-          .map(
-            (page) => `
-          <url>
-            <loc>${baseUrl}/${page}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
-          </url>
-        `
-          )
-          .join("")}
-      </urlset>
-    `;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticPages
+      .map(
+        (page) => `
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `
+      )
+      .join("")}
+  </urlset>`;
 
   // إرسال الملف كاستجابة
-  return new Response(sitemap, {
+  return new Response(sitemap.trim(), {
+    // إزالة أي فراغات زائدة
     headers: {
       "Content-Type": "application/xml",
     },
