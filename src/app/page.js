8bb2d544845717e880e34d9e154ec92beacde22e
Mostafa rayan <mr3309296@gmail.com>
2024-12-09 12:28:25 +0200

    seo v

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,12 @@ import Hero from "@/components/Home/Hero";
 import LatestProjects from "@/components/Home/LatestProjects";
 import OurCustomers from "@/components/Home/OurCustomers";
 import Services from "@/components/Home/Services";
-import Loading from "./loading";
 export const metadata = {
   title: "نوفا ميديا - الصفحة الرئيسية",
   description:
     "نوفا ميديا تقدم مجموعة شاملة من خدمات الطباعة الورقية والأقمشة، وتقنيات الليزر والفايبر، بالإضافة إلى الطباعة الداخلية والخارجية، والتسويق الإلكتروني لجعل أفكارك حقيقة.",
   keywords:
-    "نوفا ميديا, خدمات الطباعة, طباعة ورقية, طباعة الأقمشة, تقنيات الليزر, تقنيات الفايبر, الطباعة الداخلية, الطباعة الخارجية, التسويق الإلكتروني, أفضل خدمات الطباعة",
+    "نوفا ميديا,nova,novamedia,novmedia-eg,شركة دعايا واعلان, خدمات الطباعة, طباعة ورقية, طباعة الأقمشة, تقنيات الليزر, تقنيات الفايبر, الطباعة الداخلية, الطباعة الخارجية, التسويق الإلكتروني, أفضل خدمات الطباعة",
 };
 export default function Home() {
   return (
---
+++ src/app/sitemap.xml/route.js
@@ -0,0 +1,40 @@
+export async function GET() {
+  const baseUrl = "https://novamedia-eg.com"; // ضع رابط موقعك الأساسي
+
+  // الصفحات الثابتة
+  const staticPages = [
+    "services", // الصفحة الرئيسية
+    "market", // صفحة "عن الشركة"
+    "exhibitions", // صفحة "المعارض"
+    "contactus", // صفحة "اتصل بنا"
+    "team", // صفحة " فريق العمل"
+    "job-ads", // "صفحة اعلان الوظائف",
+    "site-ads", // "صفحة اعلان الوظائف",
+  ];
+
+  // إنشاء خريطة الموقع بصيغة XML
+  const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${staticPages
+          .map(
+            (page) => `
+          <url>
+            <loc>${baseUrl}/${page}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+          </url>
+        `
+          )
+          .join("")}
+      </urlset>
+    `;
+
+  // إرسال الملف كاستجابة
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
