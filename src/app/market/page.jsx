7dfe55a794a0d05edf46f1a315043c7cb6f14062
Mostafa rayan <mr3309296@gmail.com>
2024-12-13 15:58:41 +0200

    edit favicon and share img 2

--- src/app/market/page.jsx
+++ src/app/market/page.jsx
@@ -10,6 +10,11 @@ const Market = () => {
           content="yRJxt4zBwj5HVHAibjsaEKY5jV3smdQTV7bZ6FHAyGI"
         />
 
+        <meta property="og:image" content="https://novamedia-eg.com/logo.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="preload" as="image" href="/logo.png" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
         <meta property="og:url" content="https://novamedia-eg.com/" />
         {/* Meta robots */}
         <meta name="robots" content="index, follow" />
--- src/app/site-ads/page.jsx
+++ src/app/site-ads/page.jsx
@@ -17,11 +17,14 @@ const SiteAd = () => {
           name="google-site-verification"
           content="yRJxt4zBwj5HVHAibjsaEKY5jV3smdQTV7bZ6FHAyGI"
         />
-
-        <meta property="og:url" content="https://novamedia-eg.com/" />
+        <meta property="og:image" content="https://novamedia-eg.com/logo.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="preload" as="image" href="/logo.png" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
+        <meta property="og:url" content="https://novamedia-eg.com/" />{" "}
         {/* Meta robots */}
         <meta name="robots" content="index, follow" />
-
         {/* Canonical Link */}
         <link rel="canonical" href="https://novamedia-eg.com/site-ads" />
       </Head>
